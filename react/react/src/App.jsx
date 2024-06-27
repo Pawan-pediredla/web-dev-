efbcf9cc74faa2ea1174a00994f85237ede1ee6a
isafck <insaneesz1e@gmail.com>
2024-06-27 22:09:38 +0530

    usecallback

--- react/react/src/App.jsx
+++ react/react/src/App.jsx
@@ -2,16 +2,16 @@
 import './App.css'
 import Parent from './use-ref/child-component/Parent'
 import { Refusing } from './use-ref/ref'
+import { Basic } from './useCallback/basics'
+// import { Counter } from './useActionState/Counter'
 
 function App() {
- 
-
-  return (
+  return(
     <>
-      <Refusing/>
-      <Parent/>
+   <Parent/>
+   <Refusing/>
+   <Basic/>
     </>
   )
 }
-
 export default App
---
+++ react/react/src/useCallback/basics.jsx
@@ -0,0 +1,29 @@
+import { useCallback, useState } from "react"
+function Expensv({click}){
+    return(
+        <>
+        <button
+        onClick={click}
+        >
+            click
+        </button>
+        </>
+    )
+}
+export function Basic(){
+    
+    const [count,setCount] =useState(0);
+    const [value,setValue] =useState('')
+   const handleclick =useCallback(()=>{
+    setCount(count+1)
+   },[count])
+   return(
+<>
+<Expensv click={handleclick}/>
+<p>
+    {count}
+</p><p>{value}</p>
+<input value={value} onChange={(e)=>setValue(e.target.value)}/>
+</>
+    )
+}
